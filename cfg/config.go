41b4c6dd10a06731ae34e4ad151af4c838d0c1e1
maranix <ramanverma4183@gmail.com>
2024-05-22 23:21:29 +0530

    feat(cfg): use env vars from .env files for Config

--- cfg/config.go
+++ cfg/config.go
@@ -1,5 +1,10 @@
 package cfg
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Server Server `json:"server"`
 }
@@ -7,4 +12,34 @@ type Config struct {
 type Server struct {
 	Host string `json:"host"`
 	Port string `json:"port"`
+}
+
+const (
+	host = "HOST"
+	port = "PORT"
+)
+
+func create(addr string, p string) *Config {
+	return &Config{
+		Server: Server{
+			Host: addr,
+			Port: p,
+		},
+	}
+}
+
+func envVar(key string, args map[string]string) string {
+	if _, ok := args[key]; !ok {
+		fmt.Fprintf(os.Stderr, "Could not find %s in env", key)
+		os.Exit(1)
+	}
+
+	return args[key]
+}
+
+func FromArgs(args map[string]string) *Config {
+	host := envVar(host, args)
+	port := envVar(port, args)
+
+	return create(host, port)
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/maranix/ubiqhub/cfg"
 	"github.com/maranix/ubiqhub/internal/server"
 )
@@ -15,23 +16,14 @@ func main() {
 	ctx := context.Background()
 	logger := slog.Default()
 
-	host, ok := os.LookupEnv("HOST")
-	if !ok {
-		host = "0.0.0.0"
-	}
-	port, ok := os.LookupEnv("PORT")
-	if !ok {
-		port = "6969"
+	args, err := godotenv.Read()
+	if err != nil {
+		fmt.Fprintf(stderr, "Could not read env variable: %s\n", err)
+		os.Exit(1)
 	}
 
-	config := cfg.Config{
-		Server: cfg.Server{
-			Host: host,
-			Port: port,
-		},
-	}
-
-	srv := server.CreateNewServer(logger, &config)
+	config := cfg.FromArgs(args)
+	srv := server.CreateNewServer(logger, config)
 
 	if err := server.Run(ctx, logger, srv); err != nil {
 		fmt.Fprintf(stderr, "%s\n", err)
